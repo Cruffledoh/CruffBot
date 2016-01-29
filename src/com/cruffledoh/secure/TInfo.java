b0f58ac057015522550c23cbc8c4e03dc6b272e4
Ryan Youngman <ryoungman@gmail.com>
2016-01-28 21:34:58 -0800

    Initial commit.

---
+++ src/com/cruffledoh/secure/TInfo.java
@@ -0,0 +1,9 @@
+package com.cruffledoh.secure;
+
+public class TInfo {
+
+	// Put your Twitch oauth password in the 'pwd' field below
+	// it should look something like "oauth:ab3ana2lkdadn2l2k343h23n2dahye"
+	public static String pwd = "oauth:thisIsNotARealOauthPassword";
+	
+}
